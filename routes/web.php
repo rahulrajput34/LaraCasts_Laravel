4adf6ed08df4b679d7d84edf1b9a607180bebdd5
Rahul Rajput <rahul242rajput@gmail.com>
2025-01-01 08:53:36 -0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+// if I pass the file name over here It will display the ui of that
+// if I pass the string over there it will display the what ever inside the string
+// if I pass the array over there it will display the array as a json
+// Route::get('/contact', function () {
+//     return ['name' => 'Raj', 'age' => 22];
+// });
+
+
+Route::get('/about', function () {
+    return view('about');
+});
