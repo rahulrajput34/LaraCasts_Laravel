72c8b0085064f0bac76bd5fb42f378a44a2d0600
Rahul Rajput <rahul242rajput@gmail.com>
2025-01-01 16:25:58 -0500

    props vs attributes

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,6 +1,16 @@
-<a {{$attributes}}> {{$slot}} </a>
+@props(['active' => false])
+
+<a
+    class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+    aria-current="{{ $active ? 'page' : 'false' }}"
+    {{ $attributes }}
+>
+    {{ $slot }}
+</a>
 
 
-<!-- I can add any other attributes to the anchor tag over here if I want  -->
-<!-- If I want to add inside the blade component I can add attribute over there -->
-<!-- It will take attributes from the blade component and add it to the anchor tag over here and the old one remains same over here -->
+
+ {{-- <!-- aria-current="page" is an accessibility attribute that tells assistive technologies (like screen readers) that “this is the page you’re currently on. --> --}}
+  {{-- <!-- We have two thing for css one is props and second one is attributes -->  --}}
+{{-- active is a boolean prop controlling the link’s “active” styling and aria-current attribute.
+$attributes is a catch-all for any extra attributes (e.g., href, classes) passed into the component. --}}
