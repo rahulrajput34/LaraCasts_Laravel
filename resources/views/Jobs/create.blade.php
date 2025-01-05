4418ecbc27cdbff52f2770e3296eae2ee029feb7
Rahul Rajput <rahul242rajput@gmail.com>
2025-01-04 20:44:11 -0500

    Validation

--- resources/views/Jobs/create.blade.php
+++ resources/views/Jobs/create.blade.php
@@ -1,38 +1,65 @@
 <x-layout>
     <x-slot name="heading">
-        <h1 class="text-2xl font-bold text-center my-6">Create</h1>
+        <h1 class="text-2xl font-bold text-center my-6">Create Job</h1>
     </x-slot>
     <form method="POST" action="/jobs">
-      @csrf
+        @csrf
         <div class="space-y-12">
-          <div class="border-b border-gray-900/10 pb-12">
-            <h2 class="text-base/7 font-semibold text-gray-900">Create new Job</h2>
-            <p class="mt-1 text-sm/6 text-gray-600">We just need handful of details from you</p>
-            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              <div class="sm:col-span-4">
-                <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
-                <div class="mt-2">
-                  <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                    <input type="text" name="title" id="title" class="block min-w-0 grow py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder="Web Developer">
-                  </div>
-                </div>
-              </div>
+            <div class="border-b border-gray-900/10 pb-12">
+                <h2 class="text-base/7 font-semibold text-gray-900">Create new Job</h2>
+                <p class="mt-1 text-sm/6 text-gray-600">We just need handful of details from you</p>
+                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div class="sm:col-span-4">
+                        <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
+                        <div class="mt-2">
+                            <div
+                                class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                                <input type="text" name="title" id="title"
+                                    class="block min-w-0 grow py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+                                    placeholder="Web Developer"
+                                    required>
+                            </div>
+                            @error('title')
+                                <div class="text-red-500 italic text-sm/6">{{ $message }}</div>  
+                            @enderror
+                        </div>
+                    </div>
 
-              <div class="sm:col-span-4">
-                <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
-                <div class="mt-2">
-                  <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                    <input type="text" name="salary" id="salary" class="block min-w-0 grow py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder="$100000 per year">
-                  </div>
+                    <div class="sm:col-span-4">
+                        <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
+                        <div class="mt-2">
+                            <div
+                                class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                                <input type="text" name="salary" id="salary"
+                                    class="block min-w-0 grow py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+                                    placeholder="$100000 per year" required>
+                            </div>
+                            @error('salary')
+                                <div class="text-red-500 italic text-sm/6">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
                 </div>
-              </div>
+
+                {{-- This is not we gonna use --}}
+                {{-- If any validation error we get --}}
+                {{-- <div class="mt-6">
+                    @if ($errors->any())
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li class="text-red-500 italic">{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                </div> --}}
+
             </div>
-          </div>
         </div>
         <div class="mt-6 flex items-center justify-end gap-x-6">
-          <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
-          <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+            <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
+            <button type="submit"
+                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
         </div>
-      </form>
-      
+    </form>
+
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,7 @@ Route::get('/', function () {
 });
 
 
-
 Route::get('/jobs', function () {
-    // The latest will show the latest added jobs inside the database first
     $jobs = Job::with('employer')->latest()->cursorPaginate(3);   
     return view('jobs.index', [
         'jobs' => $jobs
@@ -21,27 +19,25 @@ Route::get('/jobs', function () {
 
 
 Route::post('/jobs', function () {
-    // We can get every field from the form using request()->all() method
-    // dd(request()->all());
-    // Once we get the data we can store it in the database
-   
-    // First we need to 
-    // Validate the request...
 
-    // Then we can create a new job inside the database
+    // Validate the request
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
 
+
+    // Put it in the database
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
         'employer_id' => 1
     ]);
-
     return redirect('/jobs');
 });
 
 
 
-// We pass the whatsoever routes before passing /jobs/{id} route else its give error
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
@@ -49,13 +45,11 @@ Route::get('/jobs/create', function () {
 
 
 Route::get('/jobs/{id}', function ($id) {
-    
     $job = Job::find($id);
 
     return view('jobs.show', [
         'job' => $job
     ]);
-
 });
 
 
