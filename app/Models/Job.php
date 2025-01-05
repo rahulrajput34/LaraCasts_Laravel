0d4f129a7a0e407daa1650e0e8d9171e22e8281c
Rahul Rajput <rahul242rajput@gmail.com>
2025-01-04 19:44:54 -0500

    Forms and CSRF(Cross-Site request forgery)

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -17,6 +17,7 @@ class Job extends Model
     protected $fillable = [
         'title',
         'salary',
+        'employer_id'
     ];
 
     
--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,38 @@ Route::get('/', function () {
 
 
 Route::get('/jobs', function () {
-    // TODO: this is how we can get rid of the n+1 problems
-    // job with the relationship name and then get
-    $jobs = Job::with('employer')->cursorPaginate(3);   
-    return view('jobs', [
+    // The latest will show the latest added jobs inside the database first
+    $jobs = Job::with('employer')->latest()->cursorPaginate(3);   
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
+});
+
+
+Route::post('/jobs', function () {
+    // We can get every field from the form using request()->all() method
+    // dd(request()->all());
+    // Once we get the data we can store it in the database
+   
+    // First we need to 
+    // Validate the request...
+
+    // Then we can create a new job inside the database
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
+});
+
+
+
+// We pass the whatsoever routes before passing /jobs/{id} route else its give error
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 
@@ -26,7 +52,7 @@ Route::get('/jobs/{id}', function ($id) {
     
     $job = Job::find($id);
 
-    return view('job', [
+    return view('jobs.show', [
         'job' => $job
     ]);
 
